1c7f2a2b288123727f3d5c45f9ef342406b5e4fd
iamasink <iamasink@proton.me>
2023-03-20 15:37:32 +0000

    ?

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -7,29 +7,6 @@ export default new ApplicationCommand({
 	data: new SlashCommandBuilder()
 		.setName('start')
 		.setDescription('Start a game')
-	// .addStringOption(option => option
-	// 	.setName("count")
-	// 	.setDescription("description!")
-	// 	.setRequired("true")
-	// 	.setChoices(
-	// 		{
-	// 			"name": "2v2",
-	// 			"value": "2v2"
-	// 		},
-	// 		{
-	// 			"name": "3v3",
-	// 			"value": "3v3"
-	// 		},
-	// 		{
-	// 			"name": "4v4",
-	// 			"value": "4v4"
-	// 		},
-	// 		{
-	// 			"name": "5v5",
-	// 			"value": "5v5"
-	// 		}
-	// )
-	// )
 
 	,
 	async execute(interaction: ChatInputCommandInteraction): Promise<void> {
@@ -63,6 +40,13 @@ export default new ApplicationCommand({
 					.setLabel('5v5')
 					.setStyle(ButtonStyle.Primary)
 			)
+			.addComponents(
+				new ButtonBuilder()
+					.setCustomId('any')
+					.setLabel('any')
+					.setStyle(ButtonStyle.Primary)
+			)
+
 
 		let msg1 = await interaction.reply({ embeds: embeds.messageEmbed("Choose a ghfyntmvbjcg <:redamonguspng:857630212644929556>"), fetchReply: true, components: [row1] })
 		let playerCount: number = 0
@@ -73,6 +57,7 @@ export default new ApplicationCommand({
 				i.reply({ embeds: embeds.warningEmbed(`not for you to pick you dumb shit, ask ${(interaction.member as GuildMember).displayName} to choose. twat.`), ephemeral: true })
 				return
 			}
+			msg1.delete()
 			switch (i.customId) {
 				case "2v2": {
 					playerCount = 1
@@ -90,12 +75,19 @@ export default new ApplicationCommand({
 					playerCount = 10
 					break
 				}
+				case 'any': {
+					playerCount = -1
+					break
+				}
+				default: {
+					console.log("tf?")
+				}
 			}
 
 			console.log("hi")
 
 
-			// create a reaction for users to react to in order to join the game
+			// create buttons for users to use to in order to join/leave/start the game
 			let row = new ActionRowBuilder<ButtonBuilder>()
 				.addComponents(
 					new ButtonBuilder()
@@ -123,11 +115,12 @@ export default new ApplicationCommand({
 			let msg2 = await i.reply({ embeds: embeds.messageEmbed("Game Created!", "Join by clicking the button"), fetchReply: true, components: [row] })
 			let joined: any[] = []
 
-			// const filter = reaction => reaction.
 			const collector = msg2.createMessageComponentCollector({ time: 180 * 1000 })
 			// will run on every time a reaction is collected
 			collector.on("collect", async i => {
-				switch (i.customId) {
+				const option = i.customId
+
+				switch (option) {
 					case 'join': {
 
 						const newrow = row
@@ -176,11 +169,28 @@ export default new ApplicationCommand({
 						break
 					}
 					case 'start': {
+						if (i.user.id !== interaction.user.id) {
+							i.reply({ embeds: embeds.warningEmbed(`Ask ${(interaction.member as GuildMember).displayName} to start`), ephemeral: true })
+							return
+						}
 						// checks if there is the right amount of people in the game
 						// if so it can start
 						// if not it can't
 						// (you could just do it so the start button enables when there's enough people)
+						let row = new ActionRowBuilder<ButtonBuilder>()
+							.addComponents(
+								new ButtonBuilder()
+									.setCustomId('join')
+									.setLabel('Join')
+									.setEmoji("<:watson3:971504370866876458>")
+									.setStyle(ButtonStyle.Primary),
+							)
 
+						if (joined.length < playerCount) {
+							interaction.reply("There aren't enough people!")
+						} else if (joined.length > playerCount) {
+							interaction.reply("Theres too many people!")
+						}
 
 
 						// pick one random person from the list to message telling them they're sus
